d585246576221becc248fc298ba8162fed7954a2
LeeYoHan <iyohan@LeeYoHanui-MacBookPro.local>
2018-10-12 23:26:46 +0900

    Login page complete

--- unsplash_client/src/components/Login/Login.js
+++ unsplash_client/src/components/Login/Login.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
     render() {
         return (
             <div>
-              <h1>I'm Login</h1>    
+              <FontAwesomeIcon icon='camera' size='4x' /><br />
+              <span>Login</span><br />
+              <span>Welcome back</span><br />
+              <button><FontAwesomeIcon icon='comment-dots' size='2x' />Login with Kakao Talk</button><br />
+              <span>OR</span><br />
+              <span>Email address</span><br />
+              <input type='text' /><br />
+              <span>Password</span><br />
+              <input type='text' /><br />
+              <button>Login</button><br />
+              <span>Don't have an accout?<Link to='/join'>Join</Link></span>
             </div>
         );
     }
